caddba2d88211eff4608712cbea9cf71f1903321
bwyma1 <brockdwyma@gmail.com>
2024-10-14 23:28:15 -0500

    update

--- Source.R
+++ Source.R
@@ -12,49 +12,83 @@ library("ggcorrplot")
 library("DT")
 library(dplyr)
 
-# 'Reading in the data
-cases <- read_csv("COVID-19/COVID-19_cases_plus_census.csv")
-str(cases)
-cases_tx <- read_csv("COVID-19/COVID-19_cases_TX.csv")
-str(cases_tx)
-mobility_report <- read_csv("COVID-19/Global_Mobility_Report.csv")
-str(mobility_report)
+# Reading in the data
+census <- read_csv("COVID-19/COVID-19_cases_plus_census.csv")
+census <- census[census$state == 'TX',] #Subset census to only Texas
+
+mobility <- read_csv("COVID-19/Global_Mobility_Report.csv")
+mobility <- mobility[mobility$sub_region_1 == 'Texas',]
+mobility <- mobility %>% drop_na(country_region_code) #Subset mobility report to only Texas and drop empty rows
+
+cases_TX <- read_csv("COVID-19/COVID-19_cases_TX.csv")
+cases_TX <- cases_TX[cases_TX$date >= "2020-03-05",] #March 5th 2020 is the First Case 
+str(cases_TX)
+
+counties <- read_csv("COVID-19/TX_Counties.csv")
 
 # 'Cleaning cases data
 cases <- as_tibble(cases)
-selected_columns <- c('poverty', 'walked_to_work', 'worked_at_home', 'total_pop', 
-                      'median_age', 'median_income', 'income_per_capita',
-                      'family_households', 'households', 'nonfamily_households', 
-                      'confirmed_cases', 'deaths', 'state', 'county_name')
-cases <- cases %>% select(all_of(selected_columns))
+selected_columns <- c('county_name', 'confirmed_cases', 'deaths', 'total_pop')
+
+cases_cleaned <- cases %>% select(all_of(selected_columns))
+cases_cleaned <- cases_cleaned %>%
+  rename(population = total_pop) %>%
+  rename(county = county_name) %>%
+  rename(cases = confirmed_cases) %>%
+  mutate(county = as.factor(county)) 
+age_under_21 <- cases %>% mutate(age_under_21 = rowSums(across(
+  c('male_under_5':'male_21', 'female_under_5':'female_21')))) %>%
+  select(age_under_21)
+age_22_to_34 <- cases %>% mutate(age_22_to_34 = rowSums(across(
+  c('male_22_to_24':'male_30_to_34', 'female_22_to_24':'female_30_to_34')))) %>%
+  select(age_22_to_34)
+age_35_to_64 <- cases %>% mutate(age_35_to_64 = rowSums(across(
+  c('male_35_to_39':'male_62_64', 'female_35_to_39':'female_62_to_64')))) %>%
+  select(age_35_to_64)
+age_85_and_over <- cases %>% mutate(age_85_and_over = rowSums(across(
+  c('male_65_to_66':'male_85_and_over', 'female_65_to_66':'female_85_and_over')))) %>%
+  select(age_85_and_over)
+cases_cleaned <- cases_cleaned %>% 
+  mutate(age_under_21 = age_under_21$age_under_21) %>%
+  mutate(age_22_to_34 = age_22_to_34$age_22_to_34) %>% 
+  mutate(age_35_to_64 = age_35_to_64$age_35_to_64) %>%
+  mutate(age_85_and_over = age_85_and_over$age_85_and_over)
+
+cases_cleaned <- cases_cleaned %>% 
+  mutate(age_under_21 = age_under_21/population) %>%
+  mutate(age_22_to_34 = age_22_to_34/population) %>% 
+  mutate(age_35_to_64 = age_35_to_64/population) %>%
+  mutate(age_85_and_over = age_85_and_over/population)
 
 # 'Cleaning Texas cases data
 cases_tx <- as_tibble(cases_tx)
 selected_columns <- c('county_name', 'date', 'confirmed_cases', 'deaths')
 cases_tx <- cases_tx %>% select(all_of(selected_columns))
 
+
 # 'Cleaning mobility report
-mobility_report <- as_tibble(mobility_report)
-selected_columns <- c('country_region', 'date', 
+mobility <- as_tibble(mobility)
+selected_columns <- c('sub_region_2', 'date', 
                       'retail_and_recreation_percent_change_from_baseline', 
                       'grocery_and_pharmacy_percent_change_from_baseline',
                       'parks_percent_change_from_baseline',
                       'transit_stations_percent_change_from_baseline',
-                      'workplace_percent_change_from_baseline',
+                      'workplaces_percent_change_from_baseline',
                       'residential_percent_change_from_baseline')
-mobility_report <- mobility_report %>% select(all_of(selected_columns))
+mobility <- mobility %>% select(all_of(selected_columns))
+mobility <- mobility %>% 
+  rename(county = sub_region_2)
 
 # ' 
-# Checking out the mobility report for the United States
-mobility_report_us <- mobility_report %>% filter(country_region_code == "US")
-mobility_report_us_no_region <- mobility_report_us %>% filter(is.na(sub_region_1))
+# Checking out the mobility report for the Texas Counties
+mobility <- mobility %>% drop_na(county)
 
 # Removing Weekends since people don't normally work on weekends 
-mobility_report_us_no_region <-  mobility_report_us_no_region %>%
+mobility <-  mobility %>%
   filter(!(weekdays(date) %in% c("Saturday", "Sunday")))
 
 # Plotting line chart for entire US with date and 
-ggplot(mobility_report_us_no_region) +
+ggplot(mobility) +
   geom_line(aes(x = date, y = workplaces_percent_change_from_baseline, color = "Workplace")) +
   geom_smooth(aes(x = date, y = workplaces_percent_change_from_baseline, color = "Workplace"), se = FALSE) +
   geom_line(aes(x = date, y = parks_percent_change_from_baseline, color = "Parks")) +
