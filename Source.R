42f9f7220296793039b0e9c700c594e749b9ffb8
CarsonRPittman <60508384+CarsonRPittman@users.noreply.github.com>
2024-10-14 22:07:02 -0500

    Update Source.R
    Add other read statements and subset data

--- Source.R
+++ Source.R
@@ -12,5 +12,13 @@ library("ggcorrplot")
 library("DT")
 
 # Reading in the data
-cases <- read_csv("COVID-19/COVID-19_cases_plus_census.csv")
+census <- read_csv("COVID-19/COVID-19_cases_plus_census.csv")
+census <- census[census$state == 'TX',] #Subset census to only Texas
 
+mobility <- read_csv("COVID-19/Global_Mobility_Report.csv")
+mobility <- mobility[mobility$sub_region_1 == 'Texas',]
+mobility <- mobility %>% drop_na(country_region_code) #Subset mobility report to only Texas and drop empty rows
+
+cases_TX <- read_csv("COVID-19/COVID-19_cases_TX.csv")
+cases_TX <- cases_TX[cases_TX$date >= "2020-03-05",] #March 5th 2020 is the First Case 
+str(cases_TX)
