feb9ff8636955c446c97edf9df67a02ee5f5bc4f
bwyma1 <brockdwyma@gmail.com>
2024-12-09 20:48:08 -0600

    added SVM, Naive Bayes, and ANN classifiers

--- Classification.R
+++ Classification.R
@@ -6,6 +6,10 @@ library("ggcorrplot")
 library("DT")
 library(stringr)
 library(dplyr)
+library(caret)
+library(smotefamily)
+
+set.seed(333)
 
 cases_tx <- read_csv("COVID-19/COVID-19_cases_TX.csv")
 
@@ -78,8 +82,7 @@ cases <- cases %>%
 #select(county_name, state, confirmed_cases, deaths, total_pop, median_income, median_age)
 cases <- cases %>% mutate(
   cases_per_10000 = confirmed_cases/total_pop*10000, 
-  deaths_per_10000 = deaths/total_pop*10000, 
-  death_per_case = deaths/confirmed_cases)
+  deaths_per_10000 = deaths/total_pop*10000)
 
 age_under_21 <- cases %>% mutate(age_under_21 = rowSums(across(
   c('male_under_5':'male_21', 'female_under_5':'female_21')))) %>%
@@ -106,11 +109,11 @@ cases_sel <- cases %>% select(county_name, total_pop,
                               commute_public_transportation,
                               commute_alone, age_under_21, age_22_to_64,
                               age_65_and_over,
-                              cases_per_10000, deaths_per_10000, death_per_case)
+                              cases_per_10000, deaths_per_10000)
 
 # normalize by population 
 cases_sel <- cases_sel %>% mutate(
-  commuts_public_transportation = commute_public_transportation / total_pop, 
+  commute_public_transportation = commute_public_transportation / total_pop, 
   commute_alone = commute_alone / total_pop,
   age_under_21 = age_under_21 / total_pop, 
   age_22_to_64 = age_22_to_64 / total_pop, 
@@ -148,10 +151,13 @@ cases_sel <- cases_sel %>%
 
 cases_sel <- full_join(cases_sel, hospital_data, by = "county_name") %>%
   mutate(across(where(is.numeric), ~ replace_na(., 0)),
-         hospital_beds = hospital_beds / total_pop) 
+         hospital_beds = hospital_beds / total_pop*10000) 
 
 cases_sel <- cases_sel %>%
   filter(total_pop != 0)
+
+cases_sel <- cases_sel %>%
+  select(-cases_per_10000, -deaths_per_10000)
 
 summary(cases_sel)
 
@@ -160,3 +166,200 @@ summary(cases_sel)
 #' Splitting into training and testing data sets
 #'
 #'
+
+pkgs <- c("basemodels", "C50", "caret", "e1071", "klaR", 
+          "lattice", "MASS", "mlbench", "nnet", "palmerpenguins", 
+          "randomForest", "rpart", "RWeka", "scales", "tidyverse", 
+          "xgboost")
+
+pkgs_install <- pkgs[!(pkgs %in% installed.packages()[,"Package"])]
+if(length(pkgs_install)) install.packages(pkgs_install)
+
+library(caret)
+
+cases_sel <- cases_sel %>%
+  select(-county_name)
+  
+inTrain <- createDataPartition(y = cases_sel$bad_cases, p = 0.8, list = FALSE)
+training_cases <- cases_sel[inTrain, ] %>%
+  select(-bad_deaths)
+testing_cases <- cases_sel[-inTrain, ] %>%
+  select(-bad_deaths)
+
+training_deaths <- cases_sel[inTrain, ] %>%
+  select(-bad_cases)
+testing_deaths <- cases_sel[-inTrain, ] %>%
+  select(-bad_cases)
+
+training_deaths_without_county <- training_deaths %>%
+  select(-bad_deaths)
+training_deaths <- SMOTE(training_deaths_without_county, training_deaths$bad_deaths, K=5, dup_size = (sum(training_deaths$bad_deaths == TRUE)/sum(training_deaths$bad_deaths == FALSE)))
+training_deaths <- training_deaths$data %>%
+  rename(bad_deaths = class)
+
+cases_formula <- as.formula(paste("bad_cases ~ ."))
+deaths_formula <- as.formula(paste("bad_deaths ~ ."))
+
+#'
+#'
+#' Decision Tree Classifier
+#'
+#'
+
+
+#'
+#'
+#' Rule-Based Classifier
+#'
+#'
+
+
+#'
+#'
+#' SVM
+#'
+#'
+
+svmFit1 <- training_cases |> train(cases_formula,
+                                  method = "svmLinear",
+                                  data = _,
+                                  tuneLength = 5,
+                                  trControl = trainControl(method = "cv"))
+svmFit1
+
+svmFit2 <- training_cases |> train(cases_formula,
+                                 method = "svmPoly",
+                                 data = _,
+                                 tuneLength = 5,
+                                 trControl = trainControl(method = "cv"))
+svmFit2
+plot(svmFit2)
+
+svmFit3 <- training_cases |> train(cases_formula,
+                                   method = "svmRadial",
+                                   data = _,
+                                   tuneLength = 5,
+                                   trControl = trainControl(method = "cv"))
+svmFit3
+plot(svmFit3)
+
+svmFit1$finalModel
+svmFit2$finalModel
+svmFit3$finalModel
+
+svmFit1_deaths <- training_deaths |> train(deaths_formula,
+                                   method = "svmLinear",
+                                   data = _,
+                                   tuneLength = 5,
+                                   trControl = trainControl(method = "cv"))
+svmFit1_deaths
+
+svmFit2_deaths <- training_deaths |> train(deaths_formula,
+                                   method = "svmPoly",
+                                   data = _,
+                                   tuneLength = 5,
+                                   trControl = trainControl(method = "cv"))
+svmFit2_deaths
+
+svmFit3_deaths <- training_deaths |> train(deaths_formula,
+                                           method = "svmRadial",
+                                           data = _,
+                                           tuneLength = 5,
+                                           trControl = trainControl(method = "cv"))
+svmFit3_deaths
+
+svmFit1_deaths$finalModel
+svmFit2_deaths$finalModel
+svmFit3_deaths$finalModel
+
+# Predicting on test data
+predictions <- predict(svmFit1, newdata = testing_cases)
+
+conf_matrix <- confusionMatrix(predictions, testing_cases$bad_cases)  
+print(conf_matrix)
+
+predictions <- predict(svmFit2, newdata = testing_cases)
+
+conf_matrix <- confusionMatrix(predictions, testing_cases$bad_cases)  
+print(conf_matrix)
+
+predictions <- predict(svmFit2_deaths, newdata = testing_deaths)
+
+conf_matrix <- confusionMatrix(predictions, testing_deaths$bad_deaths)  
+print(conf_matrix)
+
+#'
+#'
+#' Naive Bayes Classifier
+#'
+#'
+
+NBFit <- train(x = as.data.frame(testing_cases[, -ncol(testing_cases)]), 
+               y = pull(testing_cases, "bad_cases"),
+               method = "nb",
+               tuneGrid = data.frame(fL = c(.2, .5, 1, 5), 
+                                     usekernel = TRUE, adjust = 1),
+               trControl = trainControl(method = "cv"))
+NBFit
+
+NBFit_deaths <- train(x = as.data.frame(testing_deaths[, -ncol(testing_deaths)]), 
+               y = pull(testing_deaths, "bad_deaths"),
+               method = "nb",
+               tuneGrid = data.frame(fL = c(.2, .5, 1, 5), 
+                                     usekernel = TRUE, adjust = 1),
+               trControl = trainControl(method = "cv"))
+NBFit_deaths
+
+NBFit$finalModel$apriori
+NBFit_deaths$finalModel$apriori
+
+# Predicting on test data
+predictions <- predict(NBFit, newdata = testing_cases)
+
+conf_matrix <- confusionMatrix(predictions, testing_cases$bad_cases)  
+print(conf_matrix)
+
+predictions <- predict(NBFit_deaths, newdata = testing_deaths)
+
+conf_matrix <- confusionMatrix(predictions, testing_deaths$bad_deaths) 
+print(conf_matrix)
+
+
+#'
+#'
+#' Artificial Neural Network
+#'
+#'
+
+ANNFit <- training_cases |> train(cases_formula,
+                                   method = "nnet",
+                                   data = _,
+                                   tuneLength = 5,
+                                   trControl = trainControl(method = "cv"),
+                                   trace = FALSE)
+ANNFit
+plot(ANNFit)
+
+ANNFit_deaths <- training_deaths |> train(deaths_formula,
+                                  method = "nnet",
+                                  data = _,
+                                  tuneLength = 5,
+                                  trControl = trainControl(method = "cv"),
+                                  trace = FALSE)
+ANNFit_deaths
+plot(ANNFit_deaths)
+
+ANNFit$finalModel
+ANNFit_deaths$finalModel
+
+# Predicting on test data
+predictions <- predict(ANNFit, newdata = testing_cases)
+
+conf_matrix <- confusionMatrix(predictions, testing_cases$bad_cases)  
+print(conf_matrix)
+
+predictions <- predict(ANNFit_deaths, newdata = testing_deaths)
+
+conf_matrix <- confusionMatrix(predictions, testing_deaths$bad_deaths) 
+print(conf_matrix)
+
